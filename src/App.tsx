6f55ef39249918e6906fc9598e96c0a51221c3b2
gabriele22romano-max <gabriele22romano@gmail.com>
2025-10-29 16:26:30 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import FIFASquadCalculator from "./FIFASquadCalculator";
+
+export default function App() {
+  return (
+    <div className="App">
+      <FIFASquadCalculator />
+    </div>
+  );
+}
